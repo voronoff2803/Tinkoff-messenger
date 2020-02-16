dbceb8140b96c4ab13840f4577fd0d14b29ea97f
Алексей <voronoff2803@gmail.com>
2020-02-16 16:22:22 +0300

    Initial Commit

---
+++ Tinkoff messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinkoff messenger
+//
+//  Created by Алексей Воронов on 16.02.2020.
+//  Copyright © 2020 voronoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
