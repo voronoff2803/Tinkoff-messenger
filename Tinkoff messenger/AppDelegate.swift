2e2099cf3fd86f32ac5fd875d960054440d083a9
Алексей <voronoff2803@gmail.com>
2020-02-16 17:55:59 +0300

    #1 commit

--- Tinkoff messenger/AppDelegate.swift
+++ Tinkoff messenger/AppDelegate.swift
@@ -10,28 +10,41 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        printForTask("not running", "inactive", #function)
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        printForTask("active", "inactive", #function)
+        
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        printForTask("inactive", "active", #function)
     }
-
-
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        printForTask("inactive", "background", #function)
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        printForTask("background", "inactive", #function)
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        printForTask("background", "suspended", #function)
+    }
+    
+    func printForTask(_ from: String, _ to: String, _ method: String) {
+        #if DEBUG
+        print("Application moved from \(from) to \(to): \(method)")
+        #endif
+    }
 }
 
--- Tinkoff messenger/ViewController.swift
+++ Tinkoff messenger/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        printForTask("disappeared", "appearing", #function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        printForTask("appearing", "appeared", #function)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        printForTask("layouted", "layouting", #function)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        printForTask("layouting", "layouted", #function)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        printForTask("appeared", "disappearing", #function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        printForTask("disappearing", "disappeared", #function)
+    }
+    
+    func printForTask(_ from: String, _ to: String, _ method: String) {
+        
+        #if DEBUG
+        print("ViewController moved from \(from) to \(to): \(method)")
+        #endif
     }
 
 
 }
-
